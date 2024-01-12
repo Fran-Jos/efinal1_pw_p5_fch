a3f4f55f02d9c4508f3f104bc4cf43ae8a69ae4d
Fran-Jos <fjchanataxi@uce.edu.ec>
2024-01-11 20:01:47 -0500

    prueba 1

---
+++ efinal1_pw_p5_fch/src/components/Cuadrado.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <img :src="cargarimagen(n)" alt="No carga imagen">
+    <p>{{ txt }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img : String,
+    n: Number, // Debes recibir n como prop
+    txt:String
+  },
+  data() {
+    return {
+  
+    };
+  },
+
+  methods: {
+    cargarimagen(n) {
+      return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/" + n + ".svg";
+    },
+  },
+};
+</script>
+
+<style>
+img {
+  height: 300px;
+  width: 300px;
+}
+</style>
